9052c4b99c8aab088e5dcf6893b7a8ad6c6cb42e
SZ Quadri <szquadri@zaidsoft.com>
2014-02-28 00:00:41 +0530

    smartmontools added

--- base.sh
+++ base.sh
@@ -1,5 +1,8 @@
 # A base server should have the following stuff to make sysadmin happy
 yum install wget mc strace sysstat dstat hdparm htop iftop atop iptraf mtr jwhois mutt elinks tmux screen -y
 
+# stuff required for bare metal servers like virt hardware nodes
+yum install smartmontools -y
+
 # This repo is required to get many packages such as proftpd etc 
 rpm -Uvh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-7.noarch.rpm
