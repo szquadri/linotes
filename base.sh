d54e3b0f0beeed670c10978fec73ddee6549420f
SZ Quadri <szquadri@zaidsoft.com>
2014-07-01 00:31:24 +0530

    updated EPEL repo pkg link

--- base.sh
+++ base.sh
@@ -1,8 +1,9 @@
+# This repo is required to get many packages such as atop, iftop, socat etc 
+rpm -Uvh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+
 # A base server should have the following stuff to make sysadmin happy
 yum install wget mc nc socat strace sysstat dstat hdparm htop iftop atop iptraf mtr jwhois mutt elinks tmux screen -y
 
 # stuff required for bare metal servers like virt hardware nodes
 yum install smartmontools -y
 
-# This repo is required to get many packages such as proftpd etc 
-rpm -Uvh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-7.noarch.rpm
