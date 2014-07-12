af1e009d87dab2059596e2bed63aa2ae8a408a1f
SZ Quadri <szquadri@zaidsoft.com>
2014-07-12 15:31:18 +0530

    turn on process ac

--- base.sh
+++ base.sh
@@ -4,6 +4,8 @@ rpm -Uvh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.no
 # A base server should have the following stuff to make sysadmin happy
 yum install wget mc nc socat strace sysstat dstat psacct hdparm htop iftop atop iptraf mtr jwhois mutt elinks tmux screen -y
 
+chkconfig --level 35 psacct on
+
 # stuff required for bare metal servers like virt hardware nodes
 yum install smartmontools -y
 
