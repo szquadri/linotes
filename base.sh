1ca2cc29e4ac83b8b0ddb8e2cb399f5ad37a28a7
SZ Quadri <szquadri@zaidsoft.com>
2014-07-12 15:24:38 +0530

    added psacct

--- base.sh
+++ base.sh
@@ -2,7 +2,7 @@
 rpm -Uvh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 
 # A base server should have the following stuff to make sysadmin happy
-yum install wget mc nc socat strace sysstat dstat hdparm htop iftop atop iptraf mtr jwhois mutt elinks tmux screen -y
+yum install wget mc nc socat strace sysstat dstat psacct hdparm htop iftop atop iptraf mtr jwhois mutt elinks tmux screen -y
 
 # stuff required for bare metal servers like virt hardware nodes
 yum install smartmontools -y
