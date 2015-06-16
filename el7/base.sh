523d3240f6b4da2f9f39596e1219ae67cc3efd92
SZ Quadri <szquadri@zaidsoft.com>
2015-06-16 15:08:09 +0530

    initial version

---
+++ el7/base.sh
@@ -0,0 +1,10 @@
+# This repo is required to get many packages such as atop, iftop, socat etc 
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
+# A base server should have the following stuff to make sysadmin happy
+yum install wget mc nc socat strace sysstat dstat psacct hdparm htop iftop atop iptraf mtr jwhois mutt elinks tmux screen -y
+
+chkconfig --level 35 psacct on
+
+# stuff required for bare metal servers like virt hardware nodes
+yum install smartmontools -y
