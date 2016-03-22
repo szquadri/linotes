14e55e8e6e6b4c0d701c2f62d95809c28f2591ce
SZ Quadri <szquadri@users.noreply.github.com>
2016-03-22 16:23:24 +0530

    Do not auto mount media/other-disks (when running gnome desktop)

---
+++ nomount.sh
@@ -0,0 +1,12 @@
+
+# Do not auto mount media/other-disks (when running gnome desktop)
+
+gconftool-2 --direct \
+--config-source xml:readwrite:/etc/gconf/gconf.xml.mandatory \
+--type bool \
+--set /apps/nautilus/preferences/media_automount false
+
+gconftool-2 --direct \
+--config-source xml:readwrite:/etc/gconf/gconf.xml.mandatory \
+--type bool \
+--set /apps/nautilus/preferences/media_autorun_never true
