da6a95d73ba5934c002b6d3e3f84a06b236af86b
SZ Quadri <szquadri@users.noreply.github.com>
2016-05-24 08:34:17 +0530

    Update base.sh

--- base.sh
+++ base.sh
@@ -1,5 +1,5 @@
 # This repo is required to get many packages such as atop, iftop, socat etc 
-rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+yum install epel-release -y
 
 # A base server should have the following stuff to make sysadmin happy
 yum install wget mc nc socat strace sysstat dstat psacct hdparm htop iftop atop iptraf mtr jwhois mutt elinks tmux screen -y
